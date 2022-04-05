f7791d1bae1498c7c1f2d046dabe090bcf2b32f3
AgustinVal <53452754+AgustinVal@users.noreply.github.com>
2022-04-04 21:57:55 -0400

    Agrego movimiento a ghost intro

--- Ghost_Rider/scripts/Ghost_Intro.gd
+++ Ghost_Rider/scripts/Ghost_Intro.gd
@@ -8,6 +8,8 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	var target_vel = Input.get_action_strength("down") -	Input.get_action_strength("up")
-	linear_vel.y = lerp(linear_vel.y, target_vel * SPEED, 0.5)
+	var target_velY = Input.get_action_strength("down") -	Input.get_action_strength("up")
+	var target_velX = Input.get_action_strength("right") -	Input.get_action_strength("left")
+	linear_vel.y = lerp(linear_vel.y, target_velY * SPEED, 0.5)
+	linear_vel.x = lerp(linear_vel.x, target_velX * SPEED, 0.5)
 	linear_vel = move_and_slide(linear_vel)
